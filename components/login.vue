ede10d20cc8c69052d9d211d7130b7ca462afc70
StanAdim <stanjustine@gmail.com>
2025-05-19 09:47:04 +0300

    Initial setups

---
+++ components/login.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    Component: login
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <template>
+    <div class="min-h-screen bg-gray-100 flex items-center justify-center text-2xl text-blue-600">
+      Tailwind CSS with Nuxt 3!
+    </div>
+  </template>
+
+</template>
+
+<style scoped></style>
